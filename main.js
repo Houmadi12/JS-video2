ac91ddbc47a0ffea62b69098c9908cb545cc6c04
soilihi ambdouroihmane <soilihiambdouroihmane@gmail.com>
2024-06-10 06:48:55 +0000

    update code video 2

---
+++ main.js
@@ -0,0 +1,35 @@
+// Déclaration d'un variable
+
+// let age;
+
+var age = 14;
+
+age = 29;
+
+document.write(age+ "  : ");
+
+// age = "Jason";
+
+// document.write(age);
+// alert(age);
+
+// Pour connaitre le type de variable
+
+document.write(typeof age);
+
+var name = prompt("Comment t'appelles-tu ?");
+
+document.write("  Votre nom est : "+name);
+
+// var age = prompt("Quel age as-tu ?");
+
+// transformer le chaine de caracter en entier
+// age = parseInt(age);
+
+// document.write(typeof age);
+
+const prenom = 'Ambdou';
+
+prenom = 'Said'
+
+document.write(prenom);
